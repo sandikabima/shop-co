f29f1ca67c8740d990ac46206a73652949cb2f13
bima sandika <bimasandika@gmail.com>
2025-05-09 18:04:10 -0700

    modifed: index from server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`)
 })
