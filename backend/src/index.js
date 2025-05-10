081e3dd12dbbba13c19e3f5f5b895c208a03bed0
bima sandika <bimasandika@gmail.com>
2025-05-10 06:05:51 -0700

    modified index server

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,6 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser");
 
-const sequelize = require("./utils/conn");
-
 app.use(cookieParser())
 app.use(
     cors({
@@ -24,27 +22,10 @@ app.use(
     })
 )
 
-
-
-
 app.use(bodyParser.json())
 app.use(express.json())
 
 app.use(router)
-
-router.get('/health', async (req, res) => {
-    try {
-        await sequelize.authenticate();
-        res.status(200).json({ status: '✅ Database connected successfully' });
-    } catch (error) {
-        console.error('❌ Database connection error:', error);
-        res.status(500).json({ status: '❌ Database connection failed', error: error.message });
-    }
-});
-
-app.get("/ping", (req, res) => {
-    res.send("pong")
-})
 
 app.listen(port, () => {
     console.log(`Server listening at port ${port}`)
